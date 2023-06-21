9044943861a47a28d5e67a38f1f87b848838181c
Emily Dianni <emily256dianni@gmail.com>
2023-06-21 11:16:05 -0300

    21/06

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Incluir a classe httpresponse.
+from django.http import HttpResponse
+
+# Define uma function view chamada index.
+def index(request):
+#   return HttpResponse('Ola Django - index')
+    return render(request, 'Index.html')# novo retorno
+
+# Define uma function view chamada ola.
+def ola(request):
+    return HttpResponse
+
+
