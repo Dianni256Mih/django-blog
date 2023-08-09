076c029f32a7a39d964865f874d1c53a1ae7b762
Emily <emily256dianni@gmail.com>
2023-08-09 11:08:34 -0300

    Inclui o model Post

--- blog/views.py
+++ blog/views.py
@@ -12,4 +12,11 @@ def index(request):
 def ola(request):
     return render(request, 'home.html')
 
+from blog.models import Post # Acrescentar
+def ola(request): # Modificar
+    # return HttpResponse('Olá django')
+    posts = Post.objects.all() # recupera todos os posts do banco de dados
+    context = {'posts_list': posts } # cria um dicionário com os dado
+    return render(request, 'posts.html', context) # renderiza o template e passa o contexto
+
 
